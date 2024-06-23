54588e504fa29e5b74454d26a46c9462c45f25dc
siyanaaashatech <siyana@aashatech.com>
2024-06-23 17:26:56 +0545

    updates.

--- resources/views/portal/includes/topnav.blade.php
+++ resources/views/portal/includes/topnav.blade.php
@@ -8,8 +8,8 @@
                     </div>
                     <div class="col-md-8 d-flex align-items-center justify-content-center">
                         <p class="top_header text-center">
-                            {{ __('Bagmati Province Government') }} <br>
-                            <span class="ministry_name">{{ __('Ministry of Culture, Tourism & Coperative') }}</span><br>
+                            {{ __('Bagamati Province Government') }} <br>
+                            <span class="ministry_name">{{ __('Ministry of Culture, Tourism & Co-operatives') }}</span><br>
                             {{ __('Tourism Development Project') }}<br>
                             <span class="office_name">{{ __('Unit Office Kathmandu') }}</span><br>
                             {{ __('New Baneshwar, Nepal') }}
--- resources/views/portal/team.blade.php
+++ resources/views/portal/team.blade.php
@@ -7,37 +7,26 @@
         <h3 class="cat_title">{{ __("Employee Details") }}</h3>
         <div class="row mt-3">
             
-           
         @foreach ($teams as $team)
             <div class="col-md-4">
                 <div class="card team_card mt-2 mb-2">
-                    @if (isset($team->image))
-                    <img src="{{ asset('uploads/team/' . $team->image) }}" class="card-img-top image">
+                    @if ($team->image)
+                        <img src="{{ asset('uploads/team/' . $team->image) }}" class="card-img-top image">
                     @else
-                        <img src="{{ url('img/logo.png') }}" class="card-img-top image">
+                        <img src="{{ asset('img/logo.png') }}" class="card-img-top image">
                     @endif
                     <div class="card-body">
-                  
                         <span class="team_name">{{ __($team->name) }}</span><br>
                         <span class="team_position">{{ __($team->position) }}</span><br>
                         <span class="team_email">{{ $team->email }}</span><br>
                         <span class="team_contact">{{ $team->contact_number }}</span>
-                    
                     </div>
                 </div>
             </div>
         @endforeach
 
         </div>
-
-
-
-
     </div>
-
 </section>
-
-
-
 
 @endsection
